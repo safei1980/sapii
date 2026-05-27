83a535cd1837d62179675b5f8268477e691e4d87
safei1980 <ahmadsafei.com@gmail.com>
2026-05-27 23:52:32 +0700

    Update tampil_tanggal.js

--- tampil_tanggal.js
+++ tampil_tanggal.js
@@ -1,4 +1,4 @@
-function tampiltanggal(text)
+function tampiltanggal();
 {
 	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
 	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
