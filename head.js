a670ff8de47d3058ab24ff0a2b76cc62894889e7
safei1980 <ahmadsafei.com@gmail.com>
2026-05-29 01:31:34 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,33 +1,27 @@
-function tampiltanggal(text)
-{
-	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
-	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+  <span id='clock'/>
+<script type='text/javascript'>
+<!--
+now = new Date();
+if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
+else (a=now.getTime());
+now.setTime(a);
+var tahun= now.getFullYear ()
+var hari= now.getDay ()
+var bulan= now.getMonth ()
+var tanggal= now.getDate ()
+var hariarray=new Array("Minggu","Senin",
+"Selasa","Rabu","Kamis","Jum'at","Sabtu")
+var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember")
+document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
+</script>
+<span/>
+<script type="text/javascript">
+now = new Date();
+if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
+else (a=now.getTime());
+now.setTime(a);
+document.write("" + ((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" WIB "))
 
-	var kursusblog = new Date();
 
-	var day = kursusblog.getDay();
-	var month = kursusblog.getMonth();
-	var daym = kursusblog.getDate();
-	var year = kursusblog.getFullYear();
-
-	if (daym < 10)
-		daym = "0" + daym;
-
-var currentTime = new Date()
-  var hours = currentTime.getHours()
-  var minutes = currentTime.getMinutes()
-
-  var suffix = "WIT";
-  if (hours >= 12) {
-  suffix = "WIB";
-  hours = hours - 12;
-  }
-  if (hours == 0) {
-  hours = 12;
-  }
-
-  if (minutes < 10)
-  minutes = "0" + minutes
-
-     document.write(dayarray[day] + ", " + daym + " " + montharray[month] + " " + year);<span>
-document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>")</spa>
+//-->
+</script>
