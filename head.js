e1c706e0dd4e4681daa4fa7a991ed270f8dcfab5
safei1980 <ahmadsafei.com@gmail.com>
2026-06-04 00:12:12 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-<script type='text/javascript'>
 function date_time(id)
 {
 now = new Date();
@@ -13,13 +12,10 @@ var hariarray=new Array("Minggu","Senin",
 "Selasa","Rabu","Kamis","Jum'at","Sabtu")
 var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember")
 document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
-</script>
-<span/>
-<script type="text/javascript">
+
 now = new Date();
 if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
 else (a=now.getTime());
 now.setTime(a);
 document.write("" + ((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" WIB "))
-//-->
-</script>
+}
