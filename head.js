11ca57730b62b012b5822c508e001a37dbbfabf2
safei1980 <ahmadsafei.com@gmail.com>
2026-06-04 20:33:41 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,34 +1,22 @@
 function date_time(id)
 {
-    var a_p = "";
-    var today = new Date();
-    var curr_hour = today.getHours();
-    var curr_minute = today.getMinutes();
-    var curr_second = today.getSeconds();
-    if (curr_hour < 12) {
-        a_p = "AM";
-    } else {
-        a_p = "PM";
-    }
-    if (curr_hour == 0) {
-        curr_hour = 12;
-    }
-    if (curr_hour > 12) {
-        curr_hour = curr_hour - 12;
-    }
-    curr_hour = checkTime(curr_hour);
-    curr_minute = checkTime(curr_minute);
-    curr_second = checkTime(curr_second);
- document.getElementById('clock').innerHTML=curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
-    }
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+var currentTime = new Date()
+  var hours = currentTime.getHours()
+  var minutes = currentTime.getMinutes()
+
+  var suffix = "WIB";
+  if (hours <= 12) {
+  suffix = "WIB";
+  hours = hours - 12;
+  }
+  if (hours == 0) {
+  hours = 12;
+  }
+
+  if (minutes < 10)
+  minutes = "0" + minutes
+
+  document.write("" + hours + ":" + minutes + " " + suffix + "")
+
 }
-setInterval(showTime, 500);
-    }
-  
-//-->
