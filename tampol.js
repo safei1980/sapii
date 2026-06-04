c136da7f276fc1a1f8ff6a350399f8ad667837b7
safei1980 <ahmadsafei.com@gmail.com>
2026-06-05 02:06:59 +0700

    Create tampol.js

---
+++ tampol.js
@@ -0,0 +1,17 @@
+<script type='text/javascript'>
+function tampol()
+{
+now = new Date();
+if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
+else (a=now.getTime());
+now.setTime(a);
+var tahun= now.getFullYear ()
+var hari= now.getDay ()
+var bulan= now.getMonth ()
+var tanggal= now.getDate ()
+var hariarray=new Array("Minggu","Senin",
+"Selasa","Rabu","Kamis","Jum'at","Sabtu")
+var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember")
+document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
+ }  
+</script>
