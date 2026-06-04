0e3a716164321624b86a6f520ca28400026b61ce
safei1980 <ahmadsafei.com@gmail.com>
2026-06-04 20:51:00 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,20 +1,6 @@
 function date_time(id)
 {
-	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
-	var montharray = new Array("01","02","03","04","05","06","07","08","09","10","11","12");
-
-	var kursusblog = new Date();
-
-	var day = kursusblog.getDay();
-	var month = kursusblog.getMonth();
-	var daym = kursusblog.getDate();
-	var year = kursusblog.getFullYear();
-
-	if (daym < 10)
-		daym = "0" + daym;
 	
-        document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);
-
 var currentTime = new Date()
   var hours = currentTime.getHours()
   var minutes = currentTime.getMinutes()
