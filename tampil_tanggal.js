66fb83f0ac7c5215ca9547f32a1003f8244a5945
safei1980 <ahmadsafei.com@gmail.com>
2026-06-04 20:18:49 +0700

    Update tampiltanggal.js

--- tampil_tanggal.js
+++ tampil_tanggal.js
@@ -1,4 +1,4 @@
-function tampiltanggal();
+function tampiltanggal(text)
 {
 	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
 	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
@@ -12,23 +12,6 @@ function tampiltanggal();
 
 	if (daym < 10)
 		daym = "0" + daym;
-
-var currentTime = new Date()
-  var hours = currentTime.getHours()
-  var minutes = currentTime.getMinutes()
-
-  var suffix = "WIT";
-  if (hours >= 12) {
-  suffix = "WIB";
-  hours = hours - 12;
-  }
-  if (hours == 0) {
-  hours = 12;
-  }
-
-  if (minutes < 10)
-  minutes = "0" + minutes
-
-     document.write(dayarray[day] + ", " + daym + " " + montharray[month] + " " + year);<span>
-document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>")</spa>
+	
+        document.write(dayarray[day] + ", " + daym + " " + montharray[month] + " " + year);
 }
