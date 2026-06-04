539770dcf79ba7db66dbc83be18147e6e0798cdf
safei1980 <ahmadsafei.com@gmail.com>
2026-06-04 21:12:29 +0700

    Update tampiltanggal.js

--- tampiltanggal.js
+++ tampiltanggal.js
@@ -12,9 +12,4 @@ var hariarray=new Array("Minggu","Senin",
 "Selasa","Rabu","Kamis","Jum'at","Sabtu")
 var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember")
 document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
-now = new Date();
-if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
-else (a=now.getTime());
-now.setTime(a);
-document.write("" + ((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" WIB "))
  }
