96781b1473692fcd85c2da6567a34e1fd507a751
safei1980 <ahmadsafei.com@gmail.com>
2026-06-24 00:54:36 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -13,22 +13,5 @@ function tampol(text)
 	if (daym < 10)
 		daym = "0" + daym;
 	
-var currentTime = new Date()
-var hours = currentTime.getHours()
-var minutes = currentTime.getMinutes()
-
-  var suffix = "WIB";
-  if (hours <= 12) {
-  suffix = "WIB";
-  hours = hours - 12;
-  }
-  if (hours == 0) {
-  hours = 12;
-  }
-
-  if (minutes < 10)
-  minutes = "0" + minutes
-if (showpostdate == true) 
- document.write(dayarray[day] + ", " + daym + "/" + montharray[month] + "/" + year+" " + hours + ":" + minutes + " " + suffix + "");
-document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);
+        document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);
 }
