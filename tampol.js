bc8cc6adcfb12fe43c581b7a766db3e8b0688a29
safei1980 <ahmadsafei.com@gmail.com>
2026-06-24 23:28:54 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -1,4 +1,4 @@
-function tampol(text)
+function tampol()
 {
 	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
 	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
