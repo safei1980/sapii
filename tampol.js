be01d091035cbd23bc239c655f267df5e67d656c
safei1980 <ahmadsafei.com@gmail.com>
2026-06-24 23:11:38 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -1,9 +1,10 @@
-function tampol(text)
+function tampiltanggal(text)
 {
 	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
-	var montharray = new Array("01","02","03","04","05","06","07","08","09","10","11","12");
+	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
 
 	var kursusblog = new Date();
+
 	var day = kursusblog.getDay();
 	var month = kursusblog.getMonth();
 	var daym = kursusblog.getDate();
@@ -11,5 +12,23 @@ function tampol(text)
 
 	if (daym < 10)
 		daym = "0" + daym;
- document.write(dayarray[day] + ", " + daym + " /" + montharray[month] + " /" + year);
+
+var currentTime = new Date()
+  var hours = currentTime.getHours()
+  var minutes = currentTime.getMinutes()
+
+  var suffix = "WIT";
+  if (hours >= 12) {
+  suffix = "WIB";
+  hours = hours - 12;
+  }
+  if (hours == 0) {
+  hours = 12;
+  }
+
+  if (minutes < 10)
+  minutes = "0" + minutes
+
+     document.write(dayarray[day] + ", " + daym + " " + montharray[month] + " " + year);<span>
+document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>")</spa>
 }
