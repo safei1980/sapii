031e7a6fc3c354de8ce356bcceaa20b61b55af8d
safei1980 <ahmadsafei.com@gmail.com>
2026-06-25 22:41:15 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -30,6 +30,6 @@ function tampol(text)
   if (minutes < 10)
   minutes = "0" + minutes
 document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);        
-               document.write("<small><font color='white' padding-left='15px' 
-face='Arial'><b>" + hours + ":" + minutes + " " + suffix + "</b></font></small>")
+document.write("<a style="text-decoration:none;padding-left:15px;)" + hours + ":" + minutes + " " + suffix + "")
 }
+
