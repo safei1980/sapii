fdd956d690b67ff386dba09bf885c9c798b1efc1
safei1980 <ahmadsafei.com@gmail.com>
2026-06-25 20:32:22 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -19,5 +19,5 @@ if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
 else (a=now.getTime());
 now.setTime(a);
 document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);
-document.write(&nbsp;&nbsp;&nbsp;&nbsp;"" + ((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" WIB "))
+document.write("" +&nbsp;&nbsp;&nbsp;&nbsp;((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" WIB "))
 }
