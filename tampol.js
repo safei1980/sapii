8745c74305eaa1c724e6486a4003b0e5881e5d98
safei1980 <ahmadsafei.com@gmail.com>
2026-06-28 00:47:46 +0700

    Update tampol.js

--- tampol.js
+++ tampol.js
@@ -29,7 +29,6 @@ function tampol(text)
   }
   if (minutes < 10)
   minutes = "0" + minutes
-document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);<span>
-document.write("" + hours + ":" + minutes + " " + suffix + "")</span>
+document.write(dayarray[day] + ", " + daym+"/"+ montharray[month]+"/"+year);  document.write("" + hours + ":" + minutes + " " + suffix + "")</span>
 }
 
